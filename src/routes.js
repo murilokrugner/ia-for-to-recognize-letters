48f8b3ef949bfabf0cc6edf134333f38f1b8f4b2
murilokrugner <murilokrugner@hotmail.com>
2019-10-10 15:05:27 -0300

    model html

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
+import ImagesController from './app/controllers/ImagesController';
+
 const routes = new Router();
 
 routes.get('/', (req, res) => {
     return res.json('deu certo');
 });
+
+routes.get('/images', ImagesController.index);
 
 export default routes;
